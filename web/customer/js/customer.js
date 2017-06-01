5bd15b006e9baec2f5ea571609430269537e15b5
Sabyasachi Chowdhury <sabyasachi1005@gmail.com>
2017-06-01 16:00:36 -0400

    Final version as of June 1

--- web/customer/js/customer.js
+++ web/customer/js/customer.js
@@ -1,12 +1,26 @@
 var app = angular.module("app", []);
-app.controller("HelloController", function($scope) {
 
-  var person = {
-  firstName: "Sabyasachi",
-  lastName: "Chowdhury",
-  imageSrc: "http://google/sabya"
+app.controller("HelloController", function($scope, $http) {
+
+  var onUserComplete = function(response){
+    $scope.user = response.data;
+    $http.get($scope.user.repos_url)
+        .then(onRepos, onError);
   };
 
+  var onRepos = function(response){
+    $scope.repos = response.data;
+  }
+
+  var onError = function(reason){
+     $scope.error = "Could not fetch data !"  ;
+  };
+
+    $scope.search = function(username){
+      $http.get("http://api.github.com/users/"+username)
+        .then(onUserComplete, onError);
+
+    }
+
   $scope.message = "Hello, Sabya AngularJS";
-  $scope.person = person;
 });
